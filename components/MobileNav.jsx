d6996d2f24844b71ca1f283410f506d9cbf5fe70
Belal1khalil <belalkhalil116@gmail.com>
2025-07-24 15:54:59 +0200

    make mobie nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -35,7 +35,7 @@ const MobileNav = () => {
         <SheetContent className="flex flex-col">
             <div className='mt-32 mb-40 text-center text-2xl'>
                 <Link href="/">
-                    <h1 className='text-4xl font-semibold'>Loki <span className='text-emerald-500'>.</span></h1>
+                    <h1 className='text-4xl font-semibold'>Bk <span className='text-emerald-500'>.</span></h1>
                 </Link>
             </div>
             <nav className='flex flex-col justify-center items-center gap-8'>
